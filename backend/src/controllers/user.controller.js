77727258c15061614ee7dbb41a3a7a09d25a59e9
RoushanFW196 <roushankumar6338@gmail.com>
2022-05-17 13:12:05 +0530

    updating

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,7 @@
 
 const express=require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const router=express.Router();
 const User=require('../modals/user.model')
@@ -14,10 +16,97 @@ router.get('/', async(req,res)=>{
     }
 })
 
-router.post("/register", async(req,res)=>{
-    const newuser= await User.create(req.body);
-    res.status(201).json({user:newuser});
+// router.post("/register", async(req,res)=>{
+//     const newuser= await User.create(req.body);
+//     res.status(201).json({user:newuser});
+// })
+
+
+router.post("/register",async(req,res)=>{
+    console.log(req.body)
+
+
+  try{
+
+ const userExist= await  User.findOne({email:email});
+
+ 
+ if(userExist){
+    return res.status(404).json({err:"user already registered"})
+
+ }else{
+
+const newuser=new User({username,email,password})
+    
+ 
+await newuser.save();
+res.status(201).json({message:"user successfully registered"})
+
+ }
+
+  }catch(err){
+      res.status(404).json({err:"something went wrong"})
+  }
+});
+
+
+
+
+
+router.post("/login",async (req,res)=>{
+   const {email,password}=req.body
+  
+   try{
+
+    if(!email || !password){
+     return res.status(404).json({err:"please filled the data"})
+    }
+     
+    const userlogin= await User.findOne({email:email});
+
+    if(userlogin){
+
+        const ismatch =await bcrypt.compare(password,userlogin.password)
+    
+        let token = await userlogin.generateAuthToken();
+
+ console.log(token)
+
+    res.cookie("jwtoken",token,{
+        expires: new Date(Date.now()+2500000451),
+        httpOnly: true
+    });
+
+
+
+        if(!ismatch){
+            res.status(404).json({err:"invalid credentials"})
+        }
+       else{
+           res.status(200).json({message:"signin succesfull"})
+       }
+
+    }else{
+        res.status(404).json({err:"invalid credentials"})
+    }
+      console.log(userlogin)
+   }catch(err){
+       res.status(404).json({err:"something went wrong"})
+   }
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
 
 
 
--- backend/src/modals/user.model.js
+++ backend/src/modals/user.model.js
@@ -1,5 +1,7 @@
 
 const mongoose = require('mongoose');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
 
 const UserSchema= new mongoose.Schema({
     createdby:{type:"string",required:"true"},
@@ -11,6 +13,37 @@ const UserSchema= new mongoose.Schema({
     versionKey:false,
     timestamps:true
 })
+
+
+
+
+
+UserSchema.pre("save", async function(next){
+    console.log("password hashed")
+       if(this.isModified("password")){
+           this.password= await bcrypt.hash(this.password,10);
+          
+       }
+       next();
+   });
+   
+   
+   UserSchema.methods.generateAuthToken = async function(){
+   
+       try{
+           let generatedtoken =jwt.sign({_id:this._id},process.env.SECRET_KEY)
+         this.tokens=this.tokens.concat({token:generatedtoken});
+       
+         await this.save();
+         return generatedtoken;
+       }
+       catch(err){
+           console.error(err);
+       }
+   }   
+   
+   
+   
 
 
   const User=mongoose.model("user",UserSchema);
